bb24fb07c68e0de66c70b33190f9529c43e84434
abhin500 <abhinavmv7025@gmail.com>
2025-09-06 14:47:54 +0530

    text change

--- src/componenets/Navbar.jsx
+++ src/componenets/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className=''>  this is my navbar.....</div>
+    <div className=''>  this is my first navbar.....</div>
   )
 }
 
